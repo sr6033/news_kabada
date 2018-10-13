18ecec8da1a7e97e051813c7e30315fa92cdf112
giuseppe-dandrea <gdandrea97@gmail.com>
2018-10-13 13:41:26 +0200

    Improved front-end, addend link for the news and changed mixed indentation with tabs

--- news_scrapper.py
+++ news_scrapper.py
@@ -3,7 +3,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import os, codecs
 
-news_page = 'https://www.yahoo.com/news/'
+news_page = 'https://www.yahoo.com/news'
 page = urllib2.urlopen(news_page)
 soup = BeautifulSoup(page, 'html.parser')
 
@@ -11,27 +11,34 @@ headlines_list = soup.find_all('h3')
 
 index = 1
 news_list = []
+urls_list = []
 for headline in headlines_list:
 	#headline_link = headline.find('a')
 	#print headline_link
+	url = headline.parent.get('href')
+	if not url:
+		url = headline.find('a').get('href')
 	headline = headline.text.strip()
 	news_list.append(headline)
+	urls_list.append(news_page + url)
 	index += 1
 
 # The notifier function
 def notify(title):
-    t = '-title {!r}'.format(title)
-    m = '-message {!r}'.format(' ')
-    os.system('terminal-notifier {}'.format(' '.join([m, t])))
+	t = '-title {!r}'.format(title)
+	m = '-message {!r}'.format(' ')
+	os.system('terminal-notifier {}'.format(' '.join([m, t])))
 
-    file = codecs.open("news.html", "w", "utf-8")
-    style = "<html><head><title>Today's News</title></head> <body style='background:#ededed'> <h3>Today's News</h3> <ul>"
-    file.write(style)
-    for headline in news_list:
-    	file.write("<li>" + headline + '</li>')
-    file.write("</ul> </body>")
-    file.close()
-    os.system("open news.html")
+	file = codecs.open("news.html", "w", "utf-8")
+	style = "<html><head><title>Today's News</title>\
+			<link href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">\
+			</head> <body style='background:#ededed'> <center><h1 style=\"padding:25px\">Today's News</h1></center><div class=\"container container-fluid main-container\" style=\"padding-bottom:25px\">"
+	file.write(style)
+	for i, headline in enumerate(news_list):
+		file.write("<div class=\"card\" style=\"margin-bottom:10px\"><div class=\"card-body\"><a style=\"color:#020e65\" href=\"" + urls_list[i] + "\"><center>" + headline + "</center></a></div></div>")
+	file.write("</div></body>")
+	file.close()
+	os.system("open news.html")
 
 # Calling the function
 notify(title = "Opening today's NEWS")

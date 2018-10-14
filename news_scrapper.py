6a6eb9bdab45c1265ae2a918fef7d8e4756e1005
Prabal Pratap Singh <mechaprabal@gmail.com>
2018-10-14 17:38:48 +0530

    Edited the layout of the news items Scraping from google news

--- news_scrapper.py
+++ news_scrapper.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/python2
+
+
+import webbrowser #opening a webbrowser
 import urllib2
 from bs4 import BeautifulSoup
 import os, codecs
 
-news_page = 'https://www.yahoo.com/news/'
+
+#news_page = 'https://www.yahoo.com/news/'
+news_page = 'https://news.google.com/'
+
 page = urllib2.urlopen(news_page)
 soup = BeautifulSoup(page, 'html.parser')
 
@@ -12,11 +19,11 @@ headlines_list = soup.find_all('h3')
 index = 1
 news_list = []
 for headline in headlines_list:
-	#headline_link = headline.find('a')
-	#print headline_link
-	headline = headline.text.strip()
-	news_list.append(headline)
-	index += 1
+    #headline_link = headline.find('a')
+    #print headline_link
+    headline = headline.text.strip()
+    news_list.append(headline)
+    index += 1
 
 # The notifier function
 def notify(title):
@@ -25,13 +32,41 @@ def notify(title):
     os.system('terminal-notifier {}'.format(' '.join([m, t])))
 
     file = codecs.open("news.html", "w", "utf-8")
-    style = "<html><head><title>Today's News</title></head> <body style='background:#ededed'> <h3>Today's News</h3> <ul>"
+    style = '''
+                <html>
+                <head>
+                    <title>
+                        Today's News
+                    </title>
+                    <!--Beautification-->
+
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+    <style>
+        .list-group {
+            font-family: 'Ubuntu Mono';font-size: 22px;
+        }
+
+        #heading{
+                    font-family: 'Ubuntu';
+        }
+
+                </style>
+                </head>
+                <body style='background:#ededed'>
+                    <h1 class="jumbotron" id="heading">
+                       <center>Today's News</center>
+                    </h1>
+                    <div class="container-fluid">
+
+                        <ul class="list-group">'''
     file.write(style)
     for headline in news_list:
-    	file.write("<li>" + headline + '</li>')
-    file.write("</ul> </body>")
+        file.write('<li class="list-group-item">' + headline + '</li>')
+    file.write("</ul> </div> </body>")
     file.close()
-    os.system("open news.html")
+    #os.system("open news.html")
+    webbrowser.open_new_tab("news.html") ## Opens the generated file in new tab
 
 # Calling the function
 notify(title = "Opening today's NEWS")
